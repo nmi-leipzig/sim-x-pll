60f60e3409bdea9091389ed93c4e351058a3cead
Till Mahlburg <mahlburg@posteo.de>
2020-02-21 23:24:09 +0100

    [ADD] implement changes of period_helper in period_count and removed the period_helper module

--- period_count.v
+++ period_count.v
@@ -1,7 +1,7 @@
 /*
  * period_count.v: Measures the length of the period of the input signal.
  * author: Till Mahlburg
- * year: 2019
+ * year: 2019-2020
  * organization: Universität Leipzig
  * license: ISC
  *
@@ -11,26 +11,31 @@
 
 module period_count #(
 	/* set the precision of the period count */
-	parameter RESOLUTION = 0.1) (
+	parameter RESOLUTION = 0.01) (
 	input RST,
 	input PWRDWN,
 	input clk,
-	output reg [31:0] period_length);
+	output reg [31:0] period_length_1000);
 
 	integer period_counter;
 
 	/* count up continuously with the given resolution */
 	always begin
-		period_counter <= period_counter + 1;
-		#RESOLUTION;
+		#0.001;
+		if (RST) begin
+			period_counter <= 0;
+		end else begin
+			period_counter <= period_counter + 1;
+		end
+		#(RESOLUTION - 0.001);
 	end
 
 	/* output counted value and reset counter on every rising clk edge */
 	always @(posedge clk or posedge RST or posedge PWRDWN) begin
 		if (PWRDWN || RST) begin
-			period_length <= 0;
+			period_length_1000 <= 0;
 		end else begin
-			period_length <= (period_counter * RESOLUTION);
+			period_length_1000 <= (period_counter * RESOLUTION * 1000);
 			period_counter <= 0;
 		end
 	end
